0edd9ecad156dee67e16966bb64f65c52c8903e4
JeanFernandez <jean.fernandez_imt@outlook.com>
2021-08-09 23:06:21 +0900

    Implement password validation

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 (function () {
-  const specialChars = /\!/;
+  const letters = /[a-zA-Z]/;
+  const numbers = /[0-9]/;
+  const specials = /[ !"#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`\{\|\}~]/;
+
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
 
   const password1 = document.getElementById('password1');
   const password2 = document.getElementById('password2');
@@ -8,16 +13,30 @@
   const warning2 = document.getElementById('warning2');
   
   const passwordValidation = () => {
-    console.log(specialChars.test(password1));
-
     if (
       password1.value.length < 12 ||
-      specialChars.test(password1)
+      !letters.test(password1.value) ||
+      !numbers.test(password1.value) ||
+      !specials.test(password1.value)
       ) {
       warning1.innerHTML = "";
       const paragraph = document.createElement("p");
       paragraph.textContent = "12 or more combination of letters, numbers and symbols.";
       warning1.append(paragraph);
+    } else {
+      warning1.innerHTML = "";
+
+      if (password2.value !== password1.value) {
+        warning2.innerHTML = "";
+        const paragraph = document.createElement("p");
+        paragraph.textContent = "Oops... Passwords are not matching.";
+        warning2.append(paragraph);
+      } else {
+        warning2.innerHTML = "";
+        if(name.value !== "" && email.value !== "") {
+          document.getElementById("newAccount").reset();
+        }
+      }
     }
   };
 
