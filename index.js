bfcf2c2b5cda610ed59b4da469a52647911b81a6
JeanFernandez <jean.fernandez_imt@outlook.com>
2021-08-09 21:58:19 +0900

    First commit. Define HTML structure

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 (function () {
-  let h1= document.getElementById('title')
-  h1.innerText = "Hello World"
+  const specialChars = /\!/;
+
+  const password1 = document.getElementById('password1');
+  const password2 = document.getElementById('password2');
+
+  const warning1 = document.getElementById('warning1');
+  const warning2 = document.getElementById('warning2');
+  
+  const passwordValidation = () => {
+    console.log(specialChars.test(password1));
+
+    if (
+      password1.value.length < 12 ||
+      specialChars.test(password1)
+      ) {
+      warning1.innerHTML = "";
+      const paragraph = document.createElement("p");
+      paragraph.textContent = "12 or more combination of letters, numbers and symbols.";
+      warning1.append(paragraph);
+    }
+  };
+
+  const createButton = document.getElementById('create');
+
+  document.addEventListener("DOMContentLoaded", () => {
+    createButton.onclick = passwordValidation;
+  });
 })()
